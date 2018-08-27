862bc644dd2943726c64d8acfbf1b7f5c1463460
Alejandro Aguilar Jr <alejandro.aguilar.jr@gmail.com>
2018-08-27 18:11:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file
+function findMatching(array, name){
+  let new_array = [];
+  for (const value of array){
+    if (value.toUpperCase() === name.toUpperCase()){
+      new_array.push(value)
+    }
+  }
+  return new_array;
+}
+
+function fuzzyMatch(array, chars){
+  // console.log(array,name)
+  let new_array = [];
+  let x= chars.length;
+  for (const value of array){
+    if (value.slice(0,x).toUpperCase() === chars.toUpperCase()){
+      new_array.push(value)
+    }
+  }
+  return new_array;
+}
+
+function matchName(array_in, name){
+  let new_array = array_in.filter(function (value) { return value.name === name; });
+  return new_array;
+}
